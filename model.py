98f327208a2dc88f5d33d88875d4eeb3f99d9bf4
Geesung Oh <gsethan17@gmail.com>
2021-09-05 21:53:09 +0900

    add deeper LSTM

--- model.py
+++ model.py
@@ -136,9 +136,13 @@ class FC_AE(Model) :
 
 def get_lstm_model(n_timewindow, n_feature, latent_size) :
     model = Sequential()
-    model.add(LSTM(latent_size, input_shape=(n_timewindow, n_feature)))
+    model.add(LSTM(int(n_feature/2), input_shape=(n_timewindow, n_feature), return_sequences=True))
+    # model.add(LSTM(latent_size, input_shape=(n_timewindow, n_feature)))
+    model.add(LSTM(latent_size))
     model.add(RepeatVector(n_timewindow))
-    model.add(LSTM(latent_size, return_sequences=True))
-    model.add(TimeDistributed(Dense(n_feature)))
+    model.add(LSTM(int(n_feature/2), return_sequences=True))
+    model.add(LSTM(n_feature, return_sequences=True))
+    # model.add(LSTM(latent_size, return_sequences=True))
+    # model.add(TimeDistributed(Dense(n_feature)))
     print(model.summary())
     return model
